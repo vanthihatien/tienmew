fe30a185a7cbbc9d2352159b1275592d41085d84
vanthihatien <hatienvann@gmail.com>
2025-01-09 11:13:41 +0700

    up5

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+    has_one_attached :featured_image
     has_rich_text :description
+    validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_09_035051) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_09_040531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_09_035051) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "inventory_count"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
